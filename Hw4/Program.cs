800f658c4d1523036885412d3cb3194e71f9b1a9
bita333 <b.nkpoper33@gmail.com>
2023-05-22 19:39:01 +0430

    added namespace

--- Hw4/Program.cs
+++ Hw4/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace hw4 
+{
+class program 
+    {
+        static void Main(string[] args) 
+        {
+        
+        
+        }
+    
+    }
+}
