347666519bc43236ea2484a50513216ac08fedc3
bita333 <b.nkpoper33@gmail.com>
2023-05-23 07:49:46 +0430

    added props and stuff for creating user

--- Hw4/Program.cs
+++ Hw4/Program.cs
@@ -4,12 +4,41 @@ using System.ComponentModel;
 
 namespace hw4 
 {
-class program 
-    {
+class program
+{   
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the num of persons that you want to add");
+            int numpersons = Convert.ToInt32(Console.ReadLine());
+            Person[] people = new Person[numpersons];
+            for (int i = 0; i < numpersons; i++)
+            {
+                Console.WriteLine($"enter name of person[{(i + 1)}]");
+                string name = Console.ReadLine();
+                Console.WriteLine($"enter id of person[{(i + 1)}]");
+                int id = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"enter mobile of person[{(i + 1)}]");
+                long mobile = Convert.ToInt64(Console.ReadLine());
+                Console.WriteLine($"enter birthday date of person[{(i + 1)}] and split with /");
+                string birthdayDate = Console.ReadLine();
 
+
+                Person person = new Person(name, id, mobile, birthdayDate);
+                people[i] = person;
+            }
+            Console.Clear();
+
+            foreach (Person person in people)
+            {
+                Console.WriteLine($" person name is : {person.name} prson  {person.id}  {person.moblie} , {person.birthdayDate}");
+
+            }
+
+            Console.ReadKey();
 
 
         }
-    
+
     }
 }
--- Hw4/person.cs
+++ Hw4/person.cs
@@ -6,5 +6,22 @@ using System.Threading.Tasks;
 
 namespace Hw4
 {
+    public class Person
+    {
+        public string name { get; set; }
+        public int id { get; set; }
+        public long moblie { get; set; }
+        public string birthdayDate { get; set; }
 
+
+        public Person(string name, int id, long mobile, string birthdayDate)
+        {
+            this.name = name;
+            this.id = id;
+            this.moblie = mobile;
+            this.birthdayDate = birthdayDate;
+
+
+        }
+    }
 }
